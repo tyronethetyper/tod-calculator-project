c3fd0bf5d6de3e9f16009e492e2455523af44352
Tyrone Watts <161506619+tyronethetyper@users.noreply.github.com>
2024-10-02 11:59:27 +0200

    Create basic calculator functions

---
+++ script.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+let num_1;
+let num_2;
+let operator;
+
+function operate(operator, a, b) {
+    if (operator === "+") {
+        return add(a, b);
+    }
+    else if (operator === "-") {
+        return subtract(a, b);
+    }
+    else if (operator === "/") {
+        return divide(a, b);
+    }
+    else if (operator === "*") {
+        return multiply(a, b);
+    }
+}
